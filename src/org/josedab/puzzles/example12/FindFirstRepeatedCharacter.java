6dbdf252f1b9b76b690b5870d271a7707f205677
Jose Baena <jose@brandwatch.com>
2015-07-14 22:27:42 -0700

    Example 12 - Repeated and non repeated characters

---
+++ src/org/josedab/puzzles/example12/FindFirstRepeatedCharacter.java
@@ -0,0 +1,26 @@
+package org.josedab.puzzles.example12;
+
+public class FindFirstRepeatedCharacter {
+
+    public static void main(String[] args) {
+        System.out.println(findFirstRepeatedCharacter("stress"));
+
+    }
+
+    // O(n^2)
+    public static char findFirstRepeatedCharacter(String cad) {
+
+        char result = '?';
+        for (int i = 0; i < cad.length(); i++) {
+            char currentChar = cad.charAt(i);
+            for (int j = i + 1; j < cad.length(); j++) {
+                char candidate = cad.charAt(j);
+                if (currentChar == candidate) {
+                    result = currentChar;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/org/josedab/puzzles/example12/README.md
@@ -0,0 +1,4 @@
+# code-puzzles
+##Example 12
+Find The First Repeated Character In A String 
+Find The First Non Repeated Character In A String 
