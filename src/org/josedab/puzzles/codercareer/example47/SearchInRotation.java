1540c4e737aae19409ff06ef18f5d781602f2891
Jose Baena <jose@brandwatch.com>
2015-07-22 00:19:16 -0700

    Example 47 - Added extra example for the search in rotation

--- src/org/josedab/puzzles/codercareer/example47/SearchInRotation.java
+++ src/org/josedab/puzzles/codercareer/example47/SearchInRotation.java
@@ -7,6 +7,11 @@ public class SearchInRotation {
         System.out.println(findIndexNumber(numbers, 4));
         System.out.println(findIndexNumber2(numbers, 4));
 
+        int[] numbers2 = { 15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14 };
+
+        System.out.println(findIndexNumber(numbers2, 14));
+        System.out.println(findIndexNumber2(numbers2, 14));
+
     }
 
     // O(n)
@@ -26,23 +31,20 @@ public class SearchInRotation {
         int left = 0;
         int right = array.length - 1;
 
-        while (left < right) {
+        while (left <= right) {
             int middle = (left + right) / 2;
             if (array[middle] == n) {
                 return middle;
             }
-            if (array[middle] <= array[right]) {
-                if (array[middle] <= n && array[right] >= n) {
-                    if (array[middle] > n) {
-                        right = middle;
-                    } else {
-                        left = middle;
-                    }
+            if (array[middle] < array[right]) {
+                if (array[middle] < n && array[right] >= n) {
+                    left = middle + 1;
+                } else {
+                    right = middle;
                 }
-                return middle;
             } else {
-                if (array[middle] < n && array[right] < n) {
-                    left = middle;
+                if (array[middle] < n && array[right] <= n) {
+                    left = middle + 1;
                 } else {
                     right = middle;
                 }
